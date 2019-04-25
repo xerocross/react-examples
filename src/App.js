cd12e3e80fc9cf1f295582fa4f937549ce536cab
xerocross <nonya@business.com>
2019-04-25 05:54:42 -0400

    non-functional counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  let count = 0;
+  let increment = function() {
+    count = count + 1;
+    console.log("increment : " + count);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Counter: { count }
+      <input type="button" value="increment" onClick = {increment} />
     </div>
   );
 }
